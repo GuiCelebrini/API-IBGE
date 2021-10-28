ff619ecec55d208aff1f6f35a6b1c651c362989e
GuiCelebrini <celebrinig@gmail.com>
2021-10-28 20:03:50 -0300

    Created an AsyncTask class

---
+++ app/src/main/java/com/android/guicelebrini/apiibge/IbgeAsyncTask.java
@@ -0,0 +1,10 @@
+package com.android.guicelebrini.apiibge;
+
+import android.os.AsyncTask;
+
+public class IbgeAsyncTask extends AsyncTask<Integer, Integer, String> {
+    @Override
+    protected String doInBackground(Integer... integers) {
+        return null;
+    }
+}
--- app/src/main/java/com/android/guicelebrini/apiibge/MainActivity.java
+++ app/src/main/java/com/android/guicelebrini/apiibge/MainActivity.java
@@ -8,28 +8,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonThread, buttorStop;
 
-    IbgeApiThread apiThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonThread = findViewById(R.id.bt_start);
-        buttorStop = findViewById(R.id.bt_stop);
 
-        buttonThread.setOnClickListener(view -> {
-            apiThread = new IbgeApiThread(MainActivity.this);
-            apiThread.start();
-        });
-
-        buttorStop.setOnClickListener(view -> {
-            if (apiThread != null){
-                Log.i("Resultado", "Thread stopped");
-            }
-        });
 
     }
 }
