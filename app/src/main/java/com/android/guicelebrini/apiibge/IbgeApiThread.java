ce7a7b1e06b67bf1fe8bbdab96344e098ddf8d76
GuiCelebrini <celebrinig@gmail.com>
2021-10-28 18:00:30 -0300

    Changing MainActivity layout with IbgeApiThread

--- app/src/main/java/com/android/guicelebrini/apiibge/IbgeApiThread.java
+++ app/src/main/java/com/android/guicelebrini/apiibge/IbgeApiThread.java
@@ -1,18 +1,38 @@
 package com.android.guicelebrini.apiibge;
 
+import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
+import android.widget.Button;
 
 public class IbgeApiThread extends Thread {
+
+    private Context context;
+
+    private int i;
+
+    public IbgeApiThread(Context context){
+        this.context = context;
+    }
 
     @Override
     public void run() {
         super.run();
 
-        for (int i = 0; i < 15; i++ ){
+        for (i = 1; i <= 15; i++ ){
 
             Log.i("Resultado", "run: counter " + i);
 
+            ((Activity)context).runOnUiThread(new Runnable() {
+                public void run() {
+
+                    Button activityButton = ((Activity) context).findViewById(R.id.bt_start);
+                    activityButton.setText("Contador " + i);
+                }
+            });
+
             try {
+
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
--- app/src/main/java/com/android/guicelebrini/apiibge/MainActivity.java
+++ app/src/main/java/com/android/guicelebrini/apiibge/MainActivity.java
@@ -3,26 +3,33 @@ package com.android.guicelebrini.apiibge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonThread;
+    private Button buttonThread, buttorStop;
+
+    IbgeApiThread apiThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonThread = findViewById(R.id.bt_start_thread);
+        buttonThread = findViewById(R.id.bt_start);
+        buttorStop = findViewById(R.id.bt_stop);
 
-        buttonThread.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                IbgeApiThread apiThread = new IbgeApiThread();
-                apiThread.start();
+        buttonThread.setOnClickListener(view -> {
+            apiThread = new IbgeApiThread(MainActivity.this);
+            apiThread.start();
+        });
+
+        buttorStop.setOnClickListener(view -> {
+            if (apiThread != null){
+                Log.i("Resultado", "Thread stopped");
             }
         });
+
     }
 }
